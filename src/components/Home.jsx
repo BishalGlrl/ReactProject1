ef10631fcf2b8a6a4f3ef780403bf1e50df37bce
BishalGIrI <lrbrbs100@gmail.com>
2024-05-03 23:02:51 +0545

    First commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/index.css'
+
+const Home = () => {
+  return (
+    <div class="text-red-600" >THis is the homepage</div>
+  )
+}
+
+export default Home
